789ebd97d743d66928adadde9fdb268d84661852
Stanley Bogus <sgb3a@virginia.edu>
2014-01-22 01:55:32 -0500

    Changed youtube link.

--- danznect.cpp
+++ danznect.cpp
@@ -1,5 +1,5 @@
 /* 
- * See http://youtu.be/1W11wrZPE2E
+ * See http://youtu.be/zerMfuB1WrQ
  *
  * This is a Microsoft Kinect-using program for colorfully rendering
  * depth maps in near-real time. Excellent for dance floors - e.g. connect
